013adff86463baee1237b20292010e1733afb3ed
Kaylee <kayleenasser@gmail.com>
2023-10-03 16:33:18 -0400

    Song stops after time specified:

--- src/tracks.py
+++ src/tracks.py
@@ -1,4 +1,5 @@
 import time
+import sessions
 from pathlib import Path
 from pygame import mixer
 
@@ -13,13 +14,17 @@ class Audio:
     def Play_Audio(self): 
         path = Path(self.audio_path)
         song = path / self.name
+        start_point = (self.start_minute * 60) + self.start_second
 
         mixer.init() # Starting the mixer
         mixer.music.load(song) # Loading the song
         mixer.music.set_volume(0.7)  # Setting the volume 
-        mixer.music.play(start = (self.start_minute * 60) + self.start_second) # Starting song from second indicated
-      
+        mixer.music.play(start = start_point) # Starting song from second indicated
+        if mixer.music.get_pos == self.duration:
+            mixer.music.stop()
+            mixer.music.unload(song) # Unloads the song
         time.sleep(5)
+        
 
 # Name - string
 # Path - string
@@ -30,5 +35,5 @@ class Audio:
 
 # ----- Test stuff ------
 mypath = Path("C:/Files/Hackathon/res/test_audio/")
-audio_obj = Audio("AvatarTheme.mp3", mypath, 0, 15, 10)
+audio_obj = Audio("AvatarTheme.mp3", mypath, 0, 15, 5)
 audio_obj.Play_Audio()
