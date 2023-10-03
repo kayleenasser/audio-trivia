448b6ffbf6d661dfb48c7571b3e7977b1cece33a
Raven Sim <rsim@ourmagnet.net>
2023-10-03 18:03:21 -0400

    changed audio to tracks

--- src/tracks.py
+++ src/tracks.py
@@ -4,38 +4,38 @@ import sessions
 from pathlib import Path
 from pygame import mixer
 
-class Audio:
-    def __init__(self, name, audio_path, interval):
+class Track:
+    def __init__(self, name, track_path, interval):
         self.name = name
-        self.audio_path = audio_path
+        self.track_path = track_path
         self.interval = interval
 # Name - string
 # Path - string
 # Interval - int seconds 
 
-def Play_Audio(audio_obj): 
+def Play_Track(track_obj): 
     # ----- PLACEHOLDERS, DELETE LATER -------
     start_delay = 5
     end_delay = 60000
     # ----------------------------------------
-    path = Path(audio_obj.audio_path)
-    song = path / audio_obj.name
-    duration = int(get_audio_length(song))
+    path = Path(track_obj.track_path)
+    song = path / track_obj.name
+    duration = int(get_track_length(song))
     try:
-        start_point = random.randint(start_delay, duration - end_delay - audio_obj.interval)
+        start_point = random.randint(start_delay, duration - end_delay - track_obj.interval)
     except:
-        start_point = random.randint(0, duration - audio_obj.interval)
+        start_point = random.randint(0, duration - track_obj.interval)
 
     mixer.init() # Starting the mixer
     mixer.music.load(song) # Loading the song
     mixer.music.set_volume(0.7)  # Setting the volume 
     mixer.music.play(start = start_point) # Starting song from second indicated
-    if mixer.music.get_pos == audio_obj.interval:
+    if mixer.music.get_pos == track_obj.interval:
         mixer.music.stop()
         mixer.music.unload(song) # Unloads the song
     time.sleep(5)
 
-def get_audio_length(file_path):
+def get_track_length(file_path):
     mixer.init()
     mixer.music.load(file_path)
     length_in_seconds = mixer.Sound(file_path).get_length()
@@ -46,6 +46,6 @@ def get_audio_length(file_path):
 if __name__ == '__main__':
 # ------------ MORE TEST STUFF ------------
 	mypath = Path("res/test_audio/AvatarTheme.mp3")
-	audio_obj = Audio("AvatarTheme.mp3", mypath, 5)
-	Play_Audio(audio_obj)
+	track_obj = Track("AvatarTheme.mp3", mypath, 5)
+	Play_Track(track_obj)
 # -----------------------------------------
