0a550ce1c3f0197054c550c9c455ddb91b9d1795
Emily Tang <emilytang2002@gmail.com>
2023-10-04 11:12:32 -0400

    - added default_sessions_json() - will add sessions.json if it doesn't exist in the current directory

--- src/constants.py
+++ src/constants.py
@@ -38,3 +38,28 @@ START_DELAY_KEY = 'start_delay'
 END_DELAY_KEY = 'end_delay'
 
 DIRECTORY = 'src'
+
+DEFAULT_SESSION = {
+    "default": {
+        "audio_files": [
+            {
+                "path": "res/test_audio/1-01 The Boy in the Iceberg, The Ava.mp3",
+                "answer": "The Boy in the Iceberg"
+            },
+            {
+                "path": "res/test_audio/1-03 The Southern Air Temple.mp3",
+                "answer": "The Southern Air Temple"
+            },
+            {
+                "path": "res/test_audio/1-04 The Warriors of Kyoshi.mp3",
+                "answer": "The Warriors of Kyoshi"
+            }
+        ],
+        "settings": {
+            "interval_length": 5,
+            "increase_amount": 3,
+            "start_delay": 0,
+            "end_delay": 0
+        }
+    }
+}
